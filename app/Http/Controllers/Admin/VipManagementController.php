fe69affe8f37ea3f778443d479d8a3cced392253
yuan <yuanliang123@hotmail.com>
2018-12-17 21:30:36 +1100

    add validation

--- app/Http/Controllers/Admin/VipManagementController.php
+++ app/Http/Controllers/Admin/VipManagementController.php
@@ -11,7 +11,8 @@ class VipManagementController extends Controller{
     // admin/vipmanagement  get
     // show page
     function index(){
-        return view('admin/vip');
+        $vips = \DB::table('user')->paginate(10);
+        return view('admin.vip')->with('vips', $vips);
     }
 
     // admin/vipmanagement/create  get
@@ -25,17 +26,39 @@ class VipManagementController extends Controller{
     function store(Request $request) {
         // parse string to array
         parse_str($_POST['str'], $arr);
-        $arrInsert['username'] = $arr['username'];
-        $arrInsert['password'] = $arr['password'];
-        $arrInsert['time'] = time();
-        $arrInsert['isVip'] = '1';
-        $arrInsert['aid'] = 1;
+        $rules = [
+            'username' => 'required|unique:user|between:5,20',
+            'password' => 'required|between:6,20',
+            'repassword' => 'required|same:password',
+            'email' => 'required|email'
+        ];
+        $message = [
+            "username.required" => "username is required",
+            "username.unique" => "user exists",
+            "username.between" => "the length of username must be between 5 and 20",
+            "password.required" => "please input password",
+            "password.between" => "the length of password must be between 6 and 20",
+            "repassword.same" => "passwords are inconsistent",
+            "repassword.required" => "please input password again",
+            "email.email" => "invalid email address",
+            "email.required" => "please input email"
+        ];
 
-        if(\DB::table('user')->insert($arrInsert)) {
-            echo "1";
-        } else {
-            echo "0";
-        }
+        $validator = \Validator::make($arr, $rules, $message);
+        if($validator->passes()) {
+            $arrInsert['username'] = $arr['username'];
+            $arrInsert['password'] = $arr['password'];
+            $arrInsert['time'] = time();
+            $arrInsert['isVip'] = '1';
+            $arrInsert['aid'] = 1;
+            if(\DB::table('user')->insert($arrInsert)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }else {
+            return $validator->getMessageBag()->getMessages();
+        }        
     }
 
     // admin/vipmanagement/{vipmanagement}  put
--- resources/views/admin/vip.blade.php
+++ resources/views/admin/vip.blade.php
@@ -7,37 +7,30 @@
       <table class="table table-striped table-sm">
         <thead>
           <tr>
-            <th>#</th>
+            <th>id</th>
             <th>Username</th>
             <th>Email</th>
-            <th>Phone number</th>
+            <th>Regiser time</th>
             <th>Status</th>
             <th>Operation</th>
           </tr>
         </thead>
         <tbody>
+          @foreach($vips as $vip)
           <tr>
-            <td>1</td>
-            <td>Lorem</td>
-            <td>Lorem@gmail.com</td>
-            <td>1539889834</td>
-            <td>0</td>
+            <td>{{$vip->id}}</td>
+            <td>{{$vip->username}}</td>
+            <td>{{$vip->email}}</td>
+            <td>{{$vip->time}}</td>
+            <td>{{$vip->isVip}}</td>
             <td>
               <div class="btn-group btn-group-sm">
                 <button type="button" class="btn btn-primary">edit</button>
                 <button type="button" class="btn btn-danger">delete</button>
               </div>  
             </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>amet</td>
-            <td>amet@gmail.com</td>
-            <td>15837473877</td>
-            <td>1</td>
-            <td></td>
           </tr> 
-                                
+          @endforeach                           
         </tbody>
       </table>
       <div>
@@ -66,10 +59,12 @@
           <div class="form-group">
             <label for="username">Username</label>
             <input type="email" class="form-control" id="username" name="username" placeholder="Enter username">
+            <div id="userErr"></div>
           </div>
           <div class="form-group">
             <label for="inputEmail1">Email address</label>
             <input type="email" class="form-control" id="inputEmail1" name="email" placeholder="Enter email">
+            <div id="emailErr"></div>
           </div>
           <div class="form-group">
             <label for="phonenumber">Phone number</label>
@@ -78,10 +73,12 @@
           <div class="form-group">
             <label for="inputPassword1">Password</label>
             <input type="password" class="form-control" id="inputPassword1" name="password" placeholder="Password">
+            <div id="pwdErr"></div>
           </div>
           <div class="form-group">
             <label for="inputPassword2">Re-input Password</label>
             <input type="password" class="form-control" id="inputPassword2" name="repassword" placeholder="Re-input Password">
+            <div id="repwdErr"></div>
           </div>
           <input id="reset" type="reset" hidden>
         </form>
@@ -99,11 +96,44 @@
   function add() {
     var str = $("#formAdd").serialize();
     $.post('vipmanagement', {str: str, '_token':'{{csrf_token()}}'}, function(data){
-      if(data) {
+      if(data==1) {
+        // close modal
         $(".close").click();
+        // reset form
         $("#reset").click();
-      }else {
-
+        // clear error message
+        $('#userErr').html('');
+        $('#emailErr').html('');
+        $('#pwdErr').html('');
+        $('#repwdErr').html('');
+      }else if(data){
+        var errorMsg = '';
+        if(data.username) {
+          errorMsg = "<div class='alert alert-danger'>"+data.username+"</div>";
+          $('#userErr').html(errorMsg);
+        }else {
+          $('#userErr').html('');
+        }
+        if(data.email) {
+          errorMsg = "<div class='alert alert-danger'>"+data.email+"</div>";
+          $('#emailErr').html(errorMsg);
+        }else {
+          $('#emailErr').html('');
+        }
+        if(data.password) {
+          errorMsg = "<div class='alert alert-danger'>"+data.password+"</div>";
+          $('#pwdErr').html(errorMsg);
+        }else {
+          $('#pwdErr').html('');
+        }
+        if(data.repassword) {
+          errorMsg = "<div class='alert alert-danger'>"+data.repassword+"</div>";
+          $('#repwdErr').html(errorMsg);
+        }else {
+          $('#repwdErr').html('');
+        }
+      }else{
+        alert('add failed');
       }
     })
   }
